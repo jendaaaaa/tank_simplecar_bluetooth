ffca630f3c1609413ffc508d88d064c34f370182
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-10-26 09:07:57 +0200

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://jendaaaaa.github.io/tank_simplecar_bluetooth/](https://jendaaaaa.github.io/tank_simplecar_bluetooth/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/jendaaaaa/tank_simplecar_bluetooth** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/jendaaaaa/tank_simplecar_bluetooth** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,27 @@
+OMG_Tank.Tank_Init(Tank_version.V3)
+OMG_Tank.Stop_Motors()
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        OMG_Tank.Set_Speed(100, 100)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_1_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_DOWN){
+        OMG_Tank.Set_Speed(-100, -100)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_UP){
+        OMG_Tank.Set_Speed(0, 0)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        OMG_Tank.Set_Speed(-100, 100)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_3_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        OMG_Tank.Set_Speed(100, -100)
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_4_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    }
+})
