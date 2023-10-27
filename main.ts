82e355cfb5e91b5c6ff16584973e749c7985018e
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-10-27 11:55:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,13 +7,13 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+    if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         OMG_Tank.Set_Speed(100, 100)
     } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_1_UP) {
         OMG_Tank.Set_Speed(0, 0)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_DOWN){
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
         OMG_Tank.Set_Speed(-100, -100)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_UP){
+    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_UP) {
         OMG_Tank.Set_Speed(0, 0)
     } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
         OMG_Tank.Set_Speed(-100, 100)
