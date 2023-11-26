52b399d795b470f9d6880af3471a39e06e884d5f
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-11-26 11:20:34 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-OMG_Tank.Tank_Init(Tank_version.V3)
-OMG_Tank.Stop_Motors()
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Happy)
 })
@@ -7,21 +5,23 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         OMG_Tank.Set_Speed(100, 100)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_1_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
         OMG_Tank.Set_Speed(0, 0)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
         OMG_Tank.Set_Speed(-100, -100)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_2_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
         OMG_Tank.Set_Speed(0, 0)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
         OMG_Tank.Set_Speed(-100, 100)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_3_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_UP) {
         OMG_Tank.Set_Speed(0, 0)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
         OMG_Tank.Set_Speed(100, -100)
-    } else if (control.eventValue() === EventBusValue.MES_DPAD_BUTTON_4_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_UP) {
         OMG_Tank.Set_Speed(0, 0)
     }
 })
+OMG_Tank.Tank_Init(Tank_version.V3)
+OMG_Tank.Stop_Motors()
